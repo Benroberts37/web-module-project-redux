15e856d68309d63ea78698c9446916e8f2840b76
Benroberts37 <benrobsemail@gmail.com>
2022-03-22 13:42:27 -0600

    making some more progress

---
+++ src/reducers/favoritesReducer.js
@@ -0,0 +1,15 @@
+
+const initialState = {
+    favorites: [{title: "movie", id:1}],
+    displayFavorites: true
+}
+
+const favoritesReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default: 
+            return(state)
+    }
+}
+
+
+export default favoritesReducer;
